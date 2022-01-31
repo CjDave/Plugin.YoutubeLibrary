f7fbdcafa52066141a302a428b1f635467403917
CjDave <elumezenonso@gmail.com>
2022-01-31 12:53:00 -0600

    Version 1.2
    Handling the post methods, and handling creation of json objects

---
+++ Upcoming/Vesrion 1.2/youtube/Parameters/Base.cs
@@ -0,0 +1,71 @@
+﻿
+namespace YoutubeLibrary.youtube.Parameters
+{
+    public record Header
+    {
+        public string title { get; init; }
+        public string value { get; init; }
+    }
+    public record Pair_Value
+    {
+        public string pair { get; init; }
+        public string value { get; init; }
+    }
+    public class Body_Item
+    {
+        public string title { get; init; }
+        public string value { get; init; }
+        public List<Body_Item> values;
+        public Body_Item(string Title)
+        {
+            title = Title;
+            value = "";
+            values = new List<Body_Item>();
+        }
+
+        public Body_Item(string Title, string Value)
+        {
+            title = Title;
+            value = Value;
+            values = new List<Body_Item>();
+        }
+        public Body_Item(string Title, List<Body_Item> Values)
+        {
+            title = Title;
+            values = Values;
+            value = "";
+        }
+
+    }
+    //Body value
+    public record Body
+    {
+        public List<Body_Item> body_Items;
+        public Body()
+        {
+            body_Items = new List<Body_Item>();
+        }
+    }
+
+
+    public class RequestClass
+    {
+        public const string Youtube = "https://youtube.googleapis.com/youtube/v3/";
+        public const string Http = "HTTP/1.1";
+        public enum Method { GET, POST, DELETE, UPDATE }
+        //Header values
+        public struct Request
+        {
+            public Method method { get; set; }
+            public string resource = "";
+            public string parameter = "";
+            public string youtube = Youtube;
+            public Header[] header = null;
+            public Body body = new Body();
+            public string http = Http;
+            public string uri = "";
+            public bool Mine = true;
+
+        };
+    }
+}
---
+++ Upcoming/Vesrion 1.2/youtube/Playlists/PlaylistLists.cs
@@ -0,0 +1,94 @@
+﻿using YoutubeLibrary.util;
+using YoutubeLibrary.youtube.Parameters;
+using static YoutubeLibrary.youtube.Parameters.RequestClass;
+
+namespace YoutubeLibrary.youtube
+{
+    public class PlaylistLists
+    {
+        private Request request;
+        private string parts, parameters, mine;
+        private clientService service;
+        private string result { get; set; }
+
+        internal PlaylistLists(clientService Service)
+        {
+            request = new Request { resource = "playlists?" };
+            parts = "";
+            parameters = "";
+            service = Service;
+        }
+        //standard playlists request
+        public string getPlaylist(String[] part, bool Mine)
+        {
+            request.method = Method.GET;
+            parts = valueUtil.getPart(part);
+            mine = valueUtil.isMine(Mine);
+            request.Mine = Mine;
+            callAsync();
+            return result;
+
+        }
+        //playlists request with optional parameters
+        public string getPlaylist(String[] part, bool Mine, Parameters.Body[] body)
+        {
+            request.method = Method.GET;
+            parts = valueUtil.getPart(part);
+            mine = valueUtil.isMine(Mine);
+            request.Mine = Mine;
+            callAsync();
+            return result;
+
+        }
+        public string insertPlaylist(String title, String[] part)
+        {
+            parts = valueUtil.getPart(part);
+            request.method = Method.POST;
+
+            //create new body item for the snippets
+            Parameters.Body_Item snippet_BodyItem = new Parameters.Body_Item("snippet", new List<Body_Item>());
+            snippet_BodyItem.values.Add(new Body_Item("title", title));
+
+            //Add the body item to the body
+            request.body.body_Items.Add(snippet_BodyItem);
+            callAsync();
+            return result;
+        }
+        public string insertPlaylist(String title, String[] part, List<Body_Item> _body)
+        {
+            parts = valueUtil.getPart(part);
+            request.method = Method.POST;
+            //create new body item for the snippets
+
+            Parameters.Body_Item snippet_BodyItem = new Parameters.Body_Item("snippet", new List<Body_Item>());
+            snippet_BodyItem.values.Add(new Body_Item("title", title));
+            //Add the body item to the body
+            request.body.body_Items = _body;
+            request.body.body_Items.Add(snippet_BodyItem);
+            // string g = addBody(request.body.body_Items);
+            //g = "{" + g + "}";
+            callAsync();
+            return result;
+        }
+
+        private async Task callAsync()
+        {
+            request.parameter = parts + mine + parameters;
+            switch (request.method)
+            {
+                case Method.POST:
+                    await service.api.postApiAsync(request);
+                    break;
+                case Method.GET:
+                    if (request.parameter != null)
+                    {
+                        result = await service.api.callApiAsync(request);
+                    }
+                    break;
+            }
+            result = "Error";
+        }
+    }
+
+
+}
